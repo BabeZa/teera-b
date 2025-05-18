e29dfee22faa715884dc37043b8d60b1495e08d2
BabeZa <babe.teerapat@gmail.com>
2025-05-18 22:18:33 +0700

    Fix failed build

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 
 import InfoSection from "@/containers/info-section";
 import SkillSection from "@/containers/skill-section";
-import AboutSection from "@/containers/about-section";
+// import AboutSection from "@/containers/about-section";
 import ExperienceSection from "@/containers/exp-section";
-import ProjectSection from "@/containers/project-section";
+// import ProjectSection from "@/containers/project-section";
 import ContactSection from "@/containers/contact-section";
 
 export default function Home() {
